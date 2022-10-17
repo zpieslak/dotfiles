9c79b4587e47b1e11a6af7485507da276bc1659a
Zbigniew Pieślak <zpieslak@gmail.com>
2022-10-17 19:36:50 +0200

    Split vim into plugin
    Add pyenv
    Add user tmpfiles
    Add bin folder

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,10 +3,6 @@ import XMonad.Actions.SpawnOn
 import XMonad.Hooks.FadeWindows
 import XMonad.Util.EZConfig
 import XMonad.Util.SpawnOnce
--- import Data.Map
-
--- myMouseBindings (XConfig {XMonad.modMask = modMask}) = Data.Map.fromList $ []
--- myMouseBindings (XConfig {}) = Data.Map.fromList $ []
 
 main = xmonad $ def
   {
@@ -14,7 +10,6 @@ main = xmonad $ def
     modMask = mod4Mask,
     borderWidth = 0,
     focusFollowsMouse = False,
-    -- mouseBindings = XConfig {},
     logHook = fadeWindowsLogHook $ composeAll [
       opaque,
       isUnfocused --> transparency 0.3
@@ -24,13 +19,11 @@ main = xmonad $ def
       className =? "Alacritty" --> doShift "1",
       className =? "KeePassXC" --> doShift "2",
       className =? "Chromium" --> doShift "3",
-      className =? "Min" --> doShift "4",
-      className =? "VirtualBox Manager" --> doShift "9",
-      className =? "VirtualBox Machine" --> doShift "9"
+      className =? "Min" --> doShift "4"
     ] <+> manageSpawn <+> manageHook def,
     startupHook = do
       spawnOnce "alacritty"
-      spawnOnce "chromium gmail.com app.slack.com/client teams.microsoft.com outlook.office.com/mail"
+      spawnOnce "chromium"
       spawnOnce "min"
       spawnOnce "keepassxc"
   } `additionalKeysP` [
@@ -42,4 +35,8 @@ main = xmonad $ def
     ("<XF86AudioRaiseVolume>", spawn "~/bin/dunst/volume.sh 2dB+ unmute"),
     ("<XF86AudioMute>", spawn "~/bin/dunst/volume.sh mute"),
     ("<XF86Calculator>", spawn "mate-calculator")
+  ] `removeMouseBindings` [
+    (mod4Mask, button1),
+    (mod4Mask, button2),
+    (mod4Mask, button3)
   ]
---
+++ bin/screenshot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+maim -s -u ~/Downloads/screenshot_$(date -u +"%Y%m%dT%H%M%SZ").png
